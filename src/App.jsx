99b95bb507eca3c40133a5beeba1f65d756999eb
Shashank070105 <shashankchakraborty712005@gmail.com>
2025-08-22 22:52:39 +0530

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <Navbar />
       <main>
         <Home />
       </main>
